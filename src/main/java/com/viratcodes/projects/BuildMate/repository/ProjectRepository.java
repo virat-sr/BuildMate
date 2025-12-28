3c03fc34f1d991d6a1817193dceed1ed4243d503
Virat Srivastava <vsrivastava.dev@gmail.com>
2025-12-28 10:41:45 +0530

    Query updated

--- src/main/java/com/viratcodes/projects/BuildMate/repository/ProjectRepository.java
+++ src/main/java/com/viratcodes/projects/BuildMate/repository/ProjectRepository.java
@@ -13,16 +13,27 @@ import java.util.Optional;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("""
-             SELECT p FROM Project p
-             WHERE p.deletedAt IS NULL
-             ORDER BY p.updatedAt DESC
-             """)
+            SELECT p FROM Project p
+            WHERE p.deletedAt IS NULL
+            AND EXISTS (
+                SELECT 1 FROM ProjectMember pm
+                WHERE pm.id.userId = :userId
+                AND pm.id.projectId = p.id
+            )
+            ORDER BY p.updatedAt DESC
+            """
+    )
     List<Project> findAllAccessibleByUser(@Param("userId") Long userId);
 
     @Query("""
             SELECT p FROM Project p
             WHERE p.id = :projectId
                 AND p.deletedAt IS NULL
+                AND EXISTS (
+                    SELECT 1 FROM ProjectMember pm
+                    WHERE pm.id.userId = :userId
+                    AND pm.id.projectId = :projectId
+                )
             """)
     Optional<Project> findAccessibleProjectById(@Param("projectId") Long projectId,
                                                 @Param("userId") Long userId);
